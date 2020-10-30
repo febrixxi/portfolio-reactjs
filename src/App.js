5c72feb221a0174acbeef767618d97ab83b1f100
febrixxi <37603151+febrixxi@users.noreply.github.com>
2020-10-30 17:26:20 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Sidebar from './pages/Sidebar';
 import routes from './config/routes';
-// eslint-disable-next-line no-unused-vars
-import ThemeProvider, { ThemeContext } from './themeContext';
+import { Footer } from './pages';
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Sidebar />
-        <Switch>
-          {routes.map((route) => {
-            return (
-              <Route
-                path={route.path}
-                component={route.component}
-                key={route.path}
-              />
-            );
-          })}
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Sidebar />
+      <Switch>
+        {routes.map((route) => {
+          return (
+            <Route
+              path={route.path}
+              component={route.component}
+              key={route.path}
+            />
+          );
+        })}
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 export default App;
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,45 +1,53 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../themeContext';
+import React, { useState } from 'react';
 
 function Sidebar() {
-  const context = useContext(ThemeContext);
-  const { toggle } = context;
+  const [count, setCount] = useState(22);
+
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
 
   return (
-    <div style={{ backgroundColor: toggle ? 'black' : 'rgb(255,45,85)' }}>
-      <div id="sidebar">
-        <div className="sidebar-title">
-          <div className="sidebar-title-img">
-            <img
-              src="assets/img/nonformal.jpg"
-              alt="Profile"
-              className="img-fluid"
-            />
-          </div>
-          <div className="sidebar-title-text">Febrian Aji Pangestu</div>
+    <div id="sidebar">
+      <div className="sidebar-title">
+        <div className="sidebar-title-img">
+          <img
+            src="assets/img/nonformal.jpg"
+            alt="Profile"
+            className="img-fluid"
+          />
         </div>
-        <nav className="menu">
-          <ul>
-            <li>
-              <a href="/Intro">Home</a>
-            </li>
-            <li>
-              <a href="/About">About</a>
-            </li>
-            <li>
-              <a href="/Skills">Skills</a>
-            </li>
-            <li>
-              <a href="/Education">Education</a>
-            </li>
-            <li>
-              <a href="/Contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="copyright">© 2020 All Right Reserved</div>
+        <div className="sidebar-title-text">Febrian Aji Pangestu</div>
       </div>
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="/Intro">Home</a>
+          </li>
+          <li>
+            <a href="/About">About</a>
+          </li>
+          <li>
+            <a href="/Skills">Skills</a>
+          </li>
+          <li>
+            <a href="/Education">Education</a>
+          </li>
+          <li>
+            <a href="/Contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="state">
+        <button onClick={decrementCount}>-</button>
+        <span>Umur saya {count} tahun</span>
+        <button onClick={incrementCount}>+</button>
+      </div>
+      <div className="copyright">© 2020 All Right Reserved</div>
     </div>
   );
 }
